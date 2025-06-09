4d5427dc7877f1c81da4b26ab0a2a8de3ca5dc50
Carissaokt <144763733+Carissaokt@users.noreply.github.com>
2025-06-10 00:00:50 +0700

    nyamain header footer

--- staff_homepage.php
+++ staff_homepage.php
@@ -65,9 +65,9 @@ $workspaces = array_fill(0, 8, [
       </div>
       <nav>
         <ul class="flex items-center space-x-6">
-          <li><a href="staff_homepage.php" class="text-orange-400 font-semibold py-2">Home</a></li>
-          <li><a href="staff_daftarreservasi.php" class="py-2 text-white hover:underline">Reservation List</a></li>
-          <li><a href="mengaturworkspace.html" class="py-2 text-white hover:underline">Manage Workspace</a></li>
+          <li><a href="staff_homepage.php" class="text-orange-400 font-medium  underline-offset-4">Home</a></li>
+          <li><a href="staff_daftarreservasi.php" class="text-white hover:text-orange-400 font-medium">Reservation List</a></li>
+          <li><a href="mengaturworkspace.html" class="text-white hover:text-orange-400 font-medium">Manage Workspace</a></li>
           <li>
             <a href="login.php" class="border border-white text-white px-6 py-2 rounded-md flex items-center hover:bg-white hover:bg-opacity-10 transition-colors">
               Log Out
